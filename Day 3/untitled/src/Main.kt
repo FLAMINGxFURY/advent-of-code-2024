bef436340ce12eaa9e09e8972878f51946d490e9
FLAMINGxFURY <fury.etain.5@gmail.com>
2024-12-03 00:13:05 -0500

    Part 1 complete

--- Day 3/untitled/src/Main.kt
+++ Day 3/untitled/src/Main.kt
@@ -11,4 +11,31 @@ fun main() {
 	for (line in lines) {
 		memory += line
 	}
+
+	val result = partOne(memory)
+	println("Part 1: $result")
+}
+
+fun partOne(memory: String): Int {
+	// regex to match "mul(dd?d?,dd?d?)"
+	val mulRegex = Regex("mul\\((\\d+),(\\d+)\\)")
+
+	// step through the string and find all matches
+	val matches = mulRegex.findAll(memory)
+
+	// Convert the matches to a list of strings
+	val matchList = matches.map { it.value }.toList()
+
+	// perform the mul operation and aggregate the result
+	var result = 0
+	for (match in matchList) {
+		// value 1 is between ( and ,
+		val value1 = match.substring(match.indexOf("(") + 1, match.indexOf(",")).toInt()
+		// value 2 is between , and )
+		val value2 = match.substring(match.indexOf(",") + 1, match.indexOf(")")).toInt()
+
+		result += value1 * value2
+	}
+
+	return result
 }
