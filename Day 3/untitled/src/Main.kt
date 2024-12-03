b9092032953754ada61525e19deadb7cf0fed285
FLAMINGxFURY <fury.etain.5@gmail.com>
2024-12-03 00:24:31 -0500

    Part 2 yay

--- Day 3/untitled/src/Main.kt
+++ Day 3/untitled/src/Main.kt
@@ -12,7 +12,7 @@ fun main() {
 		memory += line
 	}
 
-	val result = partOne(memory)
+	val result = partTwo(memory)
 	println("Part 1: $result")
 }
 
@@ -35,6 +35,43 @@ fun partOne(memory: String): Int {
 		val value2 = match.substring(match.indexOf(",") + 1, match.indexOf(")")).toInt()
 
 		result += value1 * value2
+	}
+
+	return result
+}
+
+fun partTwo(memory: String): Int {
+	// regex to match "mul(dd?d?,dd?d?)"
+	val mulRegex = Regex("mul\\((\\d+),(\\d+)\\)")
+	val doRegex = Regex("do\\(\\)")
+	val dontRegex = Regex("don't\\(\\)")
+
+	// step through the string and find all matches
+	var matches = mulRegex.findAll(memory)
+	matches += doRegex.findAll(memory)
+	matches += dontRegex.findAll(memory)
+
+	// order the matches by their index
+	matches = matches.sortedBy { it.range.first }
+
+	// Convert the matches to a list of strings
+	val matchList = matches.map { it.value }.toList()
+
+	// perform the mul operation and aggregate the result
+	var result = 0
+	var doFlag = true
+	for (match in matchList) {
+		if (match == "do()") doFlag = true
+		else if (match == "don't()") doFlag = false
+
+		else if (doFlag) {
+			// value 1 is between ( and ,
+			val value1 = match.substring(match.indexOf("(") + 1, match.indexOf(",")).toInt()
+			// value 2 is between , and )
+			val value2 = match.substring(match.indexOf(",") + 1, match.indexOf(")")).toInt()
+
+			result += value1 * value2
+		}
 	}
 
 	return result
