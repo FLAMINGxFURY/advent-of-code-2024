c6b44db48c8c3f4bfe83dafbdbcd561d28e7180f
FLAMINGxFURY <fury.etain.5@gmail.com>
2024-12-05 21:34:14 -0500

    I love my technically incorrect but couldn't be bothered to change it adjacency matrix.

--- Day 5/AdventOfCodeDay5/src/Main.kt
+++ Day 5/AdventOfCodeDay5/src/Main.kt
@@ -23,7 +23,8 @@ fun main() {
 	//Items after the blank line are the print orders
 	val printOrders = lines.drop(ruleLines.size + 1)
 
-	val result = partOne(rules, printOrders)
+	//val result = partOne(rules, printOrders)
+	val result = partTwo(rules, printOrders)
 	println(result)
 }
 
@@ -59,3 +60,81 @@ fun partOne(rules: Map<Int, List<Int>>, printOrders: List<String>): Int{
 	return correctListMiddleValues.sum()
 }
 
+fun findIncorrectLists(rules: Map<Int, List<Int>>, printOrders: List<String>): List<MutableList<Int>> {
+	val incorrectOrders = mutableListOf<MutableList<Int>>()
+
+	for (order in printOrders) {
+		val items = order.split(",").map { it.toInt() }
+
+		var failedOrder = false
+
+		//For each value in the order, ensure nothing preceding that value is in its mustProceed list
+		for (i in 1..<items.size) {
+			val item = items[i]
+			val mustProceed = rules[item] ?: continue
+
+			//If any of the preceding values are in the mustProceed list, the order is invalid
+			for (j in 0..<i) {
+				if (mustProceed.contains(items[j])) {
+					failedOrder = true
+					break
+				}
+			}
+		}
+
+		//If the order is invalid, add it to the list
+		if (failedOrder) {
+			incorrectOrders.add(items.toMutableList())
+		}
+	}
+
+	return incorrectOrders
+}
+
+//Depth-first search to find the post-order traversal of the subgraph
+fun dfs(page: Int, subgraph: Map<Int, List<Int>>, visited: MutableSet<Int>, postOrder: MutableList<Int>) {
+	visited.add(page)
+
+	for (mustProceed in subgraph[page] ?: return) {
+		if (mustProceed !in visited) {
+			dfs(mustProceed, subgraph, visited, postOrder)
+		}
+	}
+
+	postOrder.add(page)
+}
+
+fun partTwo(rules: Map<Int, List<Int>>, printOrders: List<String>): Int {
+	val incorrectOrders = findIncorrectLists(rules, printOrders)
+	val correctedMiddleValues = mutableListOf<Int>()
+
+	for (order in incorrectOrders) {
+		//Using rules as an adjacency matrix, create a subgraph that represents only rules for this order.
+		val subgraph = mutableMapOf<Int, MutableList<Int>>()
+
+		for((page, mustProceed) in rules) {
+			if (page in order) {
+				//Only include rules that are in the order
+				subgraph[page] = mustProceed.filter { it in order }.toMutableList()
+			}
+		}
+
+		//Using a depth-first search, we can ensure all rules are followed.
+		//The post-order traversal of the DFS will be the corrected order.
+		val visited = mutableSetOf<Int>()
+		val postOrder = mutableListOf<Int>()
+
+		for (page in order) {
+			if (page !in visited) {
+				dfs(page, subgraph, visited, postOrder)
+			}
+		}
+
+		//To convert this to a post-order traversal, we need to reverse the list
+		postOrder.reverse()
+
+		correctedMiddleValues.add(postOrder[postOrder.size / 2])
+	}
+
+	return correctedMiddleValues.sum()
+}
