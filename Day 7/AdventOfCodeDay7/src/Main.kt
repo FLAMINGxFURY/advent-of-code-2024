de6285283085ea54a32345096291e343eae642b6
FLAMINGxFURY <fury.etain.5@gmail.com>
2024-12-07 02:36:55 -0500

    Day 7 setup

---
+++ Day 7/AdventOfCodeDay7/src/Main.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+fun main() {
+    val reader = File("input.txt").bufferedReader()
+    val lines = reader.readLines()
+
+    val resultMap = mutableMapOf<Int, List<Int>>()
+
+    // separate input into result: list of int
+    for (line in lines) {
+        val parts = line.split(": ")
+        val result = parts[0].toInt()
+        val inputs = parts[1].split(" ").map { it.toInt() }
+        resultMap[result] = inputs
+    }
+}
+
+fun add(a: Int, b: Int): Int = a + b
+fun mul(a: Int, b: Int): Int = a * b
+
+
